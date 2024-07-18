674b391e6da33fb492fc2ddaa0e542a0f47d4125
hyy1987 <779003812@qq.com>
2024-07-18 16:28:36 +0000

    feat: Add new page translations and update label prop type in select components

--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -3,9 +3,11 @@ import globalHeader from './en-US/globalHeader';
 import menu from './en-US/menu';
 import pages from './en-US/pages';
 import pages_contact from './en-US/pages_contact';
-import pages_flowproperties from './en-US/pages_flowproperties';
-import pages_flows from './en-US/pages_flows';
+import pages_flow from './en-US/pages_flow';
+import pages_flowproperty from './en-US/pages_flowproperty';
 import pages_general from './en-US/pages_general';
+import pages_process from './en-US/pages_process';
+import pages_source from './en-US/pages_source';
 import pages_unitgroup from './en-US/pages_unitgroup';
 import pwa from './en-US/pwa';
 import settingDrawer from './en-US/settingDrawer';
@@ -19,16 +21,18 @@ export default {
   'app.preview.down.block': 'Download this page to your local project',
   'app.welcome.link.fetch-blocks': 'Get all block',
   'app.welcome.link.block-list': 'Quickly build standard, pages based on `block` development',
+  ...pages,
   ...globalHeader,
   ...menu,
   ...settingDrawer,
   ...settings,
   ...pwa,
   ...component,
-  ...pages,
   ...pages_general,
+  ...pages_process,
   ...pages_contact,
   ...pages_unitgroup,
-  ...pages_flowproperties,
-  ...pages_flows,
+  ...pages_flowproperty,
+  ...pages_flow,
+  ...pages_source,
 };
---
+++ src/locales/en-US/pages_process.ts
@@ -0,0 +1,18 @@
+export default {
+  'pages.process.referenceYear': 'Year',
+  'pages.process.location': 'Location',
+
+  'pages.process.drawer.title.create': 'Create Process',
+  'pages.process.drawer.title.view': 'View Process',
+  'pages.process.drawer.title.edit': 'Edit Process',
+
+  'pages.process.exchange.exchangeDirection': 'Direction',
+  'pages.process.exchange.resultingAmount': 'Resulting Amount',
+  'pages.process.exchange.meanAmount': 'Mean Amount',
+  'pages.process.exchange.quantitativeReference': 'Quantitative Reference',
+  'pages.process.exchange.dataDerivationTypeStatus': 'Data Derivation Type',
+
+  'pages.process.exchange.drawer.title.create': 'Create Exchange',
+  'pages.process.exchange.drawer.title.view': 'View Exchange',
+  'pages.process.exchange.drawer.title.edit': 'Edit Exchange',
+};
