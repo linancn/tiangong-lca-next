7c1b762960b881ce137ff8a44c84ffa31d6c919a
hyy1987 <779003812@qq.com>
2024-07-19 03:44:47 +0000

    chore: Update select components to disable input fields and improve styling

--- src/pages/Flowproperties/Components/select/from.tsx
+++ src/pages/Flowproperties/Components/select/from.tsx
@@ -1,8 +1,7 @@
 import { getFlowpropertyDetail } from '@/services/flowproperties/api';
 import { genFlowpropertyFromData } from '@/services/flowproperties/util';
-import { langOptions } from '@/services/general/data';
 import { ProFormInstance } from '@ant-design/pro-components';
-import { Button, Card, Col, Divider, Form, Input, Row, Select, Space } from 'antd';
+import { Button, Card, Col, Divider, Form, Input, Row, Space } from 'antd';
 import React, { FC, ReactNode, useEffect, useState } from 'react';
 import { FormattedMessage } from 'umi';
 import FlowpropertyView from '../view';
@@ -43,7 +42,7 @@ const FlowpropertiesSelectFrom: FC<Props> = ({ name, label, lang, formRef, onDat
     <Card size="small" title={label}>
       <Space direction="horizontal">
         <Form.Item label="Ref Object Id" name={[...name, '@refObjectId']}>
-          <Input disabled={true} style={{ width: '300px' }} />
+          <Input disabled={true} style={{ width: '350px', color:'#000' }}/>
         </Form.Item>
         <Space direction="horizontal" style={{ marginTop: '6px' }}>
           <FlowpropertiesSelectDrawer
@@ -65,10 +64,10 @@ const FlowpropertiesSelectFrom: FC<Props> = ({ name, label, lang, formRef, onDat
         </Space>
       </Space>
       <Form.Item label="Type" name={[...name, '@type']}>
-        <Input disabled={true} />
+        <Input disabled={true} style={{ color: '#000' }}/>
       </Form.Item>
       <Form.Item label="URI" name={[...name, '@uri']}>
-        <Input disabled={true} />
+        <Input disabled={true} style={{ color: '#000' }}/>
       </Form.Item>
       {/* <Form.Item label="Version" name={[...name, '@version']}>
         <Input disabled={true} />
@@ -86,19 +85,14 @@ const FlowpropertiesSelectFrom: FC<Props> = ({ name, label, lang, formRef, onDat
             <div style={{ display: 'flex', flexDirection: 'column', rowGap: 16 }}>
               {subFields.map((subField) => (
                 <Row key={subField.key}>
-                  <Col flex="120px" style={{ marginRight: '10px' }}>
+                  <Col flex="100px" style={{ marginRight: '10px' }}>
                     <Form.Item noStyle name={[subField.name, '@xml:lang']}>
-                      <Select
-                        placeholder="Select a lang"
-                        optionFilterProp="lang"
-                        options={langOptions}
-                        disabled={true}
-                      />
+                      <Input disabled={true} style={{ width: '100px', color: '#000' }} />
                     </Form.Item>
                   </Col>
                   <Col flex="auto" style={{ marginRight: '10px' }}>
                     <Form.Item noStyle name={[subField.name, '#text']}>
-                      <TextArea placeholder="text" rows={1} disabled={true} />
+                      <TextArea placeholder="text" rows={1} disabled={true} style={{ color: '#000' }} />
                     </Form.Item>
                   </Col>
                 </Row>
