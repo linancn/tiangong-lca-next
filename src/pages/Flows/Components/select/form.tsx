5bec95881bbeab6a44296f9d1321fd6101a4dfa7
hyy1987 <779003812@qq.com>
2024-09-27 15:53:22 +0000

    Refactor form components in select forms

--- src/pages/Flows/Components/select/form.tsx
+++ src/pages/Flows/Components/select/form.tsx
@@ -2,7 +2,7 @@ import UnitGroupFromMini from '@/pages/Unitgroups/Components/select/formMini';
 import { getFlowDetail } from '@/services/flows/api';
 import { genFlowFromData } from '@/services/flows/util';
 import { ProFormInstance } from '@ant-design/pro-components';
-import { Button, Card, Col, Divider, Form, Input, Row, Space } from 'antd';
+import { Button, Card, Col, Divider, Form, Input, Row, Space, theme } from 'antd';
 import React, { FC, ReactNode, useEffect, useState } from 'react';
 import { FormattedMessage } from 'umi';
 import FlowsView from '../view';
@@ -20,6 +20,7 @@ type Props = {
 
 const FlowsSelectForm: FC<Props> = ({ name, label, lang, formRef, drawerVisible, onData }) => {
   const [id, setId] = useState<string | undefined>(undefined);
+  const { token } = theme.useToken();
 
   const handletFlowsData = (rowKey: any) => {
     getFlowDetail(rowKey).then(async (result: any) => {
@@ -55,7 +56,7 @@ const FlowsSelectForm: FC<Props> = ({ name, label, lang, formRef, drawerVisible,
           }
           name={[...name, '@refObjectId']}
         >
-          <Input disabled={true} style={{ width: '350px', color: '#000' }} />
+          <Input disabled={true} style={{ width: '350px', color: token.colorTextDescription }} />
         </Form.Item>
         <Space direction="horizontal" style={{ marginTop: '6px' }}>
           <FlowsSelectDrawer buttonType="text" lang={lang} onData={handletFlowsData} />
@@ -76,13 +77,13 @@ const FlowsSelectForm: FC<Props> = ({ name, label, lang, formRef, drawerVisible,
         label={<FormattedMessage id="pages.process.view.exchange.type" defaultMessage="Type" />}
         name={[...name, '@type']}
       >
-        <Input disabled={true} style={{ color: '#000' }} />
+        <Input disabled={true} style={{ color: token.colorTextDescription }} />
       </Form.Item>
       <Form.Item
         label={<FormattedMessage id="pages.process.view.exchange.uri" defaultMessage="URI" />}
         name={[...name, '@uri']}
       >
-        <Input disabled={true} style={{ color: '#000' }} />
+        <Input disabled={true} style={{ color: token.colorTextDescription }} />
       </Form.Item>
       <Divider orientationMargin="0" orientation="left" plain>
         <FormattedMessage
@@ -98,7 +99,7 @@ const FlowsSelectForm: FC<Props> = ({ name, label, lang, formRef, drawerVisible,
                 <Row key={subField.key}>
                   <Col flex="100px" style={{ marginRight: '10px' }}>
                     <Form.Item noStyle name={[subField.name, '@xml:lang']}>
-                      <Input disabled={true} style={{ width: '100px', color: '#000' }} />
+                      <Input disabled={true} style={{ width: '100px', color: token.colorTextDescription }} />
                     </Form.Item>
                   </Col>
                   <Col flex="auto" style={{ marginRight: '10px' }}>
@@ -107,7 +108,7 @@ const FlowsSelectForm: FC<Props> = ({ name, label, lang, formRef, drawerVisible,
                         placeholder="text"
                         rows={1}
                         disabled={true}
-                        style={{ color: '#000' }}
+                        style={{ color: token.colorTextDescription }}
                       />
                     </Form.Item>
                   </Col>
