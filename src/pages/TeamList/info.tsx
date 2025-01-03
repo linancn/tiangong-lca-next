fb57d3cd3793daed2061e8c49e0d457c2f82940f
Huimin Chang <huiminchang@mail.bnu.edu.cn>
2025-01-03 15:57:35 +0800

    change the number and sort of team

--- src/pages/TeamList/info.tsx
+++ src/pages/TeamList/info.tsx
@@ -1,5 +1,38 @@
 export const Teams = [
   {
+    id: '95fa5ba5-b0d3-4d1c-95a8-40270e2399d3,bd982725-532e-4851-9940-51499a2109af',
+    title: [
+      {
+        '@xml:lang': 'zh',
+        '#text': '中国矿业大学（北京） 冯翠洋团队',
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': 'Cuiyang Feng Team, China University of Mining & Technology, Beijing ',
+      },
+    ],
+    description: [
+      {
+        '@xml:lang': 'zh',
+        '#text': (
+          <>
+            天工计划团队，从事化石能源领域的资源管理和环境效益分析研究，负责能源领域（煤炭、石油、天然气）的开放数据研制。
+          </>
+        ),
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': (
+          <>
+            Life Cycle Open Data development teams for the energy sector (coal, oil, natural gas).
+          </>
+        ),
+      },
+    ],
+    lightLogo: 'cumtb.svg',
+    darkLogo: 'cumtb-dark.svg',
+  },
+  {
     id: '9c4751b6-b31c-477a-944f-cd5b47576835',
     title: [
       {
@@ -33,42 +66,6 @@ export const Teams = [
     ],
     lightLogo: 'hiq.svg',
     darkLogo: 'hiq-dark.svg',
-  },
-  {
-    id: 'a8397fcd-6d7a-4779-a6db-5806541e6df3,035e2a24-47b5-4327-8158-4d13b82dabd5,91d3aefe-e4a8-411b-b63b-ffc3f3fe3f3c,5385953c-7557-4f2b-8359-ab3d62dce597,0b86dd09-76e1-418f-88ec-8f5ed5519cb0,1c7e995b-4c55-4204-8977-3ef020e758ed,dbcf5d8a-60bb-4dfc-a2b3-e8b4ab9352c0',
-    title: [
-      {
-        '@xml:lang': 'zh',
-        '#text': '清华大学 徐明团队',
-      },
-      {
-        '@xml:lang': 'en',
-        '#text': 'Ming Xu Team, Tsinghua University',
-      },
-    ],
-    description: [
-      {
-        '@xml:lang': 'zh',
-        '#text': (
-          <>
-            天工计划团队，负责电力系统（火电、光伏、风电、水电、核电、输配电）、能源领域（氢能、电池）、矿产领域（铝、硅）、包装材料行业的开放数据研制。
-          </>
-        ),
-      },
-      {
-        '@xml:lang': 'en',
-        '#text': (
-          <>
-            Life Cycle Open Data development teams in the power system (thermal power, photovoltaic,
-            wind power, hydropower, nuclear power, transmission and distribution), energy field
-            (hydrogen energy, batteries), mineral field (aluminium, silicon), packaging materials
-            industry.
-          </>
-        ),
-      },
-    ],
-    lightLogo: 'thu.svg',
-    darkLogo: 'thu-dark.svg',
   },
   {
     id: '91b8bb31-96c8-4191-8e16-b9090a7fc5a9',
@@ -105,39 +102,6 @@ export const Teams = [
     darkLogo: 'usst-dark.svg',
   },
   {
-    id: '95fa5ba5-b0d3-4d1c-95a8-40270e2399d3,bd982725-532e-4851-9940-51499a2109af',
-    title: [
-      {
-        '@xml:lang': 'zh',
-        '#text': '中国矿业大学（北京） 冯翠洋团队',
-      },
-      {
-        '@xml:lang': 'en',
-        '#text': 'Cuiyang Feng Team, China University of Mining & Technology, Beijing ',
-      },
-    ],
-    description: [
-      {
-        '@xml:lang': 'zh',
-        '#text': (
-          <>
-            天工计划团队，从事化石能源领域的资源管理和环境效益分析研究，负责能源领域（煤炭、石油、天然气）的开放数据研制。
-          </>
-        ),
-      },
-      {
-        '@xml:lang': 'en',
-        '#text': (
-          <>
-            Life Cycle Open Data development teams for the energy sector (coal, oil, natural gas).
-          </>
-        ),
-      },
-    ],
-    lightLogo: 'cumtb.svg',
-    darkLogo: 'cumtb-dark.svg',
-  },
-  {
     id: '79e7f7c7-4b38-4d2e-a550-c76011d1001c',
     title: [
       {
@@ -171,6 +135,42 @@ export const Teams = [
     ],
     lightLogo: 'sues.svg',
     darkLogo: 'sues-dark.svg',
+  },
+  {
+    id: 'a8397fcd-6d7a-4779-a6db-5806541e6df3,035e2a24-47b5-4327-8158-4d13b82dabd5,91d3aefe-e4a8-411b-b63b-ffc3f3fe3f3c,5385953c-7557-4f2b-8359-ab3d62dce597,0b86dd09-76e1-418f-88ec-8f5ed5519cb0,1c7e995b-4c55-4204-8977-3ef020e758ed,dbcf5d8a-60bb-4dfc-a2b3-e8b4ab9352c0',
+    title: [
+      {
+        '@xml:lang': 'zh',
+        '#text': '清华大学 徐明团队',
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': 'Ming Xu Team, Tsinghua University',
+      },
+    ],
+    description: [
+      {
+        '@xml:lang': 'zh',
+        '#text': (
+          <>
+            天工计划团队，负责电力系统（火电、光伏、风电、水电、核电、输配电）、能源领域（氢能、电池）、矿产领域（铝、硅）、包装材料行业的开放数据研制。
+          </>
+        ),
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': (
+          <>
+            Life Cycle Open Data development teams in the power system (thermal power, photovoltaic,
+            wind power, hydropower, nuclear power, transmission and distribution), energy field
+            (hydrogen energy, batteries), mineral field (aluminium, silicon), packaging materials
+            industry.
+          </>
+        ),
+      },
+    ],
+    lightLogo: 'thu.svg',
+    darkLogo: 'thu-dark.svg',
   },
   {
     id: 'ee842dac-8314-4c71-9d6a-1ce85475c198',
