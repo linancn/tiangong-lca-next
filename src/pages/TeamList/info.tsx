61db498a4389a33839ebf83db2d976de9e16c964
Huimin Chang <huiminchang@mail.bnu.edu.cn>
2025-01-03 18:44:27 +0800

    Update team information and logos in TeamList component for accuracy and consistency.

--- src/pages/TeamList/info.tsx
+++ src/pages/TeamList/info.tsx
@@ -1,14 +1,14 @@
 export const Teams = [
   {
-    id: '95fa5ba5-b0d3-4d1c-95a8-40270e2399d3,bd982725-532e-4851-9940-51499a2109af',
+    id: 'ee842dac-8314-4c71-9d6a-1ce85475c198',
     title: [
       {
         '@xml:lang': 'zh',
-        '#text': '中国矿业大学（北京） 冯翠洋团队',
+        '#text': '东北大学 岳强 团队',
       },
       {
         '@xml:lang': 'en',
-        '#text': 'Cuiyang Feng Team, China University of Mining & Technology, Beijing ',
+        '#text': 'Qiang Yue Team, Northeastern University',
       },
     ],
     description: [
@@ -16,7 +16,7 @@ export const Teams = [
         '@xml:lang': 'zh',
         '#text': (
           <>
-            天工计划团队，从事化石能源领域的资源管理和环境效益分析研究，负责能源领域（煤炭、石油、天然气）的开放数据研制。
+            聚焦工业生态学、金属流程工业节能减排理论与技术研究，负责钢铁领域（钢铁生产及废钢回收）的开放数据研制。
           </>
         ),
       },
@@ -24,13 +24,50 @@ export const Teams = [
         '@xml:lang': 'en',
         '#text': (
           <>
-            Life Cycle Open Data development teams for the energy sector (coal, oil, natural gas).
+            Focuses on industrial ecology and the theory and technology of energy-saving and
+            emission reduction in metal process industries and engaged in open data development for
+            the steel sector, including steel production and scrap steel recycling.
           </>
         ),
       },
     ],
-    lightLogo: 'cumtb.svg',
-    darkLogo: 'cumtb-dark.svg',
+    lightLogo: 'neu.svg',
+    darkLogo: 'neu-dark.svg',
+  },
+  {
+    id: '67d2e923-29de-4537-8d8b-c5df6e2ee618',
+    title: [
+      {
+        '@xml:lang': 'zh',
+        '#text': '东华大学 李斌 团队',
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': 'Bin Li Team, Donghua University',
+      },
+    ],
+    description: [
+      {
+        '@xml:lang': 'zh',
+        '#text': (
+          <>
+            聚焦高分子材料在整个生产、加工、使用以及废弃循环过程中关键低碳技术的研究与应用，负责再生塑料领域的开放数据研制。
+          </>
+        ),
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': (
+          <>
+            Focuses on the research and application of key low-carbon technologies in the entire
+            lifecycle of polymer materials, including production, processing, usage, and recycling
+            and engaged in open data development for the recycled plastics sector.
+          </>
+        ),
+      },
+    ],
+    lightLogo: 'dhu.svg',
+    darkLogo: 'dhu-dark.svg',
   },
   {
     id: '9c4751b6-b31c-477a-944f-cd5b47576835',
@@ -49,7 +86,7 @@ export const Teams = [
         '@xml:lang': 'zh',
         '#text': (
           <>
-            HiQLCD由上海海科数据智能科技有限公司开发，1.0版本数据集包含超过10,000条详实的数据，覆盖能源（石油、煤炭、电力、天然气）、基础原材料（钢铁、铜、铝、塑料、橡胶）、及下游产品（电气设备、加工机械）等领域。
+            由上海海科数据智能科技有限公司研制，1.0版本数据集包含超过10,000条详实的数据，覆盖能源（石油、煤炭、电力、天然气）、基础原材料（钢铁、铜、铝、塑料、橡胶）、及下游产品（电气设备、加工机械）等领域。
           </>
         ),
       },
@@ -57,9 +94,9 @@ export const Teams = [
         '@xml:lang': 'en',
         '#text': (
           <>
-            HiQLCD, developed by Shanghai HiQ Smart Data Co., Ltd, provides over 10,000 high
-            quality, up-to-date data sets covering critical areas such as energy production, raw
-            materials, manufacturing processes and downstream products.
+            Developed by Shanghai HiQ Smart Data Co., Ltd, provides over 10,000 high quality,
+            up-to-date data sets covering critical areas such as energy production, raw materials,
+            manufacturing processes and downstream products.
           </>
         ),
       },
@@ -68,15 +105,15 @@ export const Teams = [
     darkLogo: 'hiq-dark.svg',
   },
   {
-    id: '91b8bb31-96c8-4191-8e16-b9090a7fc5a9',
+    id: 'a8397fcd-6d7a-4779-a6db-5806541e6df3,035e2a24-47b5-4327-8158-4d13b82dabd5,91d3aefe-e4a8-411b-b63b-ffc3f3fe3f3c,5385953c-7557-4f2b-8359-ab3d62dce597,0b86dd09-76e1-418f-88ec-8f5ed5519cb0,1c7e995b-4c55-4204-8977-3ef020e758ed,dbcf5d8a-60bb-4dfc-a2b3-e8b4ab9352c0',
     title: [
       {
         '@xml:lang': 'zh',
-        '#text': '上海理工大学 黄蓓佳团队',
+        '#text': '清华大学 徐明 团队',
       },
       {
         '@xml:lang': 'en',
-        '#text': 'Beijia Huang Team, University of Shanghai  for Science and Technology ',
+        '#text': 'Ming Xu Team, Tsinghua University',
       },
     ],
     description: [
@@ -84,7 +121,7 @@ export const Teams = [
         '@xml:lang': 'zh',
         '#text': (
           <>
-            天工计划团队，从事建筑行业低碳与可持续发展方向的研究，负责建材领域（砂石、砖块、石灰、玻璃）的开放数据研制。
+            聚焦产业生态学、生命周期评价方法和工具研究，负责电力系统（火电、光伏、风电、水电、核电、输配电）、能源领域（氢能、电池）、矿产领域（铝、硅）、包装材料行业的开放数据研制。
           </>
         ),
       },
@@ -92,21 +129,24 @@ export const Teams = [
         '@xml:lang': 'en',
         '#text': (
           <>
-            Life Cycle Open Data development teams for building materials products (sand, gravel,
-            bricks, lime, glass).
+            Focuses on industrial ecology, life cycle assessment methods and tools, and engaged in
+            open data development for the power system (thermal power, photovoltaic, wind power,
+            hydropower, nuclear power, transmission and distribution), energy sector (hydrogen
+            energy, batteries), mineral sector (aluminum, silicon), and packaging materials
+            industry.
           </>
         ),
       },
     ],
-    lightLogo: 'usst.svg',
-    darkLogo: 'usst-dark.svg',
+    lightLogo: 'thu.svg',
+    darkLogo: 'thu-dark.svg',
   },
   {
     id: '79e7f7c7-4b38-4d2e-a550-c76011d1001c',
     title: [
       {
         '@xml:lang': 'zh',
-        '#text': '上海工程技术大学 碳中和系统工程课题组',
+        '#text': '上海工程技术大学 碳中和系统工程 团队',
       },
       {
         '@xml:lang': 'en',
@@ -119,7 +159,7 @@ export const Teams = [
         '@xml:lang': 'zh',
         '#text': (
           <>
-            天工计划团队，从事废弃物化学循环、石油化工生产等领域的碳足迹评估研究，负责废弃物处理领域、石油化工领域（甲醇、乙酸）的开放数据研制。
+            聚焦废弃物化学循环、石油化工生产等领域的碳足迹评估研究，负责废弃物处理领域、石油化工领域（甲醇、乙酸）的开放数据研制。
           </>
         ),
       },
@@ -127,8 +167,9 @@ export const Teams = [
         '@xml:lang': 'en',
         '#text': (
           <>
-            Life Cycle Open Data development teams for waste management and petrochemicals
-            (methanol, acetic acid).
+            Focuses on carbon footprint assessment in waste chemical recycling and petrochemical
+            production, and engaged in open data development for the waste treatment and
+            petrochemical sectors (methanol, acetic acid).
           </>
         ),
       },
@@ -137,15 +178,15 @@ export const Teams = [
     darkLogo: 'sues-dark.svg',
   },
   {
-    id: 'a8397fcd-6d7a-4779-a6db-5806541e6df3,035e2a24-47b5-4327-8158-4d13b82dabd5,91d3aefe-e4a8-411b-b63b-ffc3f3fe3f3c,5385953c-7557-4f2b-8359-ab3d62dce597,0b86dd09-76e1-418f-88ec-8f5ed5519cb0,1c7e995b-4c55-4204-8977-3ef020e758ed,dbcf5d8a-60bb-4dfc-a2b3-e8b4ab9352c0',
+    id: '91b8bb31-96c8-4191-8e16-b9090a7fc5a9',
     title: [
       {
         '@xml:lang': 'zh',
-        '#text': '清华大学 徐明团队',
+        '#text': '上海理工大学 黄蓓佳 团队',
       },
       {
         '@xml:lang': 'en',
-        '#text': 'Ming Xu Team, Tsinghua University',
+        '#text': 'Beijia Huang Team, University of Shanghai  for Science and Technology ',
       },
     ],
     description: [
@@ -153,7 +194,7 @@ export const Teams = [
         '@xml:lang': 'zh',
         '#text': (
           <>
-            天工计划团队，负责电力系统（火电、光伏、风电、水电、核电、输配电）、能源领域（氢能、电池）、矿产领域（铝、硅）、包装材料行业的开放数据研制。
+            聚焦建筑行业低碳与可持续发展方向的研究，负责建材领域（砂石、砖块、石灰、玻璃）的开放数据研制。
           </>
         ),
       },
@@ -161,57 +202,22 @@ export const Teams = [
         '@xml:lang': 'en',
         '#text': (
           <>
-            Life Cycle Open Data development teams in the power system (thermal power, photovoltaic,
-            wind power, hydropower, nuclear power, transmission and distribution), energy field
-            (hydrogen energy, batteries), mineral field (aluminium, silicon), packaging materials
-            industry.
+            Focuses on low-carbon and sustainable development in the construction industry and
+            engaged in open data development for the building materials sector (sand, bricks, lime,
+            glass).
           </>
         ),
       },
     ],
-    lightLogo: 'thu.svg',
-    darkLogo: 'thu-dark.svg',
-  },
-  {
-    id: 'ee842dac-8314-4c71-9d6a-1ce85475c198',
-    title: [
-      {
-        '@xml:lang': 'zh',
-        '#text': '东北大学 岳强团队',
-      },
-      {
-        '@xml:lang': 'en',
-        '#text': 'Qiang Yue Team, Northeastern University',
-      },
-    ],
-    description: [
-      {
-        '@xml:lang': 'zh',
-        '#text': (
-          <>
-            天工计划团队，从事工业生态学、金属流程工业节能减排理论与技术研究，负责钢铁领域（钢铁生产及废钢回收）的开放数据研制。
-          </>
-        ),
-      },
-      {
-        '@xml:lang': 'en',
-        '#text': (
-          <>
-            Life Cycle Open Data development teams in the iron and steel sector (iron and steel
-            production and steel scrap recycling).
-          </>
-        ),
-      },
-    ],
-    lightLogo: 'neu.svg',
-    darkLogo: 'neu-dark.svg',
+    lightLogo: 'usst.svg',
+    darkLogo: 'usst-dark.svg',
   },
   {
     id: 'ecab975e-440e-463d-aa95-b0f21f63afc5',
     title: [
       {
         '@xml:lang': 'zh',
-        '#text': '生态环境部环境规划院 张红振团队',
+        '#text': '生态环境部环境规划院 张红振 团队',
       },
       {
         '@xml:lang': 'en',
@@ -222,8 +228,40 @@ export const Teams = [
       {
         '@xml:lang': 'zh',
         '#text': (
+          <>聚焦绿色修复与可持续土地利用研究，负责修复领域（土壤与地下水修复）的开放数据研制。</>
+        ),
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': (
           <>
-            天工计划团队，从事绿色修复与可持续土地利用研究，负责修复领域（土壤与地下水修复）的开放数据研制。
+            Focuses on green remediation and sustainable land use research, and engaged in open data
+            development for the field of remediation (soil and groundwater).
+          </>
+        ),
+      },
+    ],
+    lightLogo: 'caep.svg',
+    darkLogo: 'caep-dark.svg',
+  },
+  {
+    id: '95fa5ba5-b0d3-4d1c-95a8-40270e2399d3,bd982725-532e-4851-9940-51499a2109af',
+    title: [
+      {
+        '@xml:lang': 'zh',
+        '#text': '中国矿业大学（北京） 冯翠洋 团队',
+      },
+      {
+        '@xml:lang': 'en',
+        '#text': 'Cuiyang Feng Team, China University of Mining & Technology, Beijing ',
+      },
+    ],
+    description: [
+      {
+        '@xml:lang': 'zh',
+        '#text': (
+          <>
+            聚焦化石能源领域的资源管理和环境效益分析研究，负责能源领域（煤炭、石油、天然气）的开放数据研制。
           </>
         ),
       },
@@ -231,13 +269,13 @@ export const Teams = [
         '@xml:lang': 'en',
         '#text': (
           <>
-            Life Cycle Open Data development teams in the field of remediation (soil and groundwater
-            remediation).
+            Focuses on resource management and environmental benefit analysis in the fossil energy
+            sector, engaged in open data development for the energy sector (coal, oil, natural gas).
           </>
         ),
       },
     ],
-    lightLogo: 'caep.svg',
-    darkLogo: 'caep-dark.svg',
+    lightLogo: 'cumtb.svg',
+    darkLogo: 'cumtb-dark.svg',
   },
 ];
