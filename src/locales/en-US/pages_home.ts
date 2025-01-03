21c7521fcb071191a6b85599d3fcc3d8af83dff3
Huimin Chang <huiminchang@mail.bnu.edu.cn>
2025-01-03 15:56:28 +0800

    update the team list description and home page description.

--- src/locales/en-US/pages_home.ts
+++ src/locales/en-US/pages_home.ts
@@ -3,7 +3,7 @@ export default {
   'pages.welcome': 'Welcome to TianGong LCA Data Platform',
   'pages.card.title.1': 'Traceable & Crediable',
   'pages.card.description.1': 'Advanced technologies and strict quality control ensure that the data sources are clear and the modeling process is transparent and traceable, thus providing users with reliable life cycle data.',
-  'pages.card.title.2': 'Integrity & Representativeness',
+  'pages.card.title.2': 'Completeness & Representativeness',
   'pages.card.description.2': 'The platform gathers comprehensive and authoritative life cycle data, covering diverse scenarios, and strives to present the real industry trends and environmental impacts.',
   'pages.card.title.3': 'Internationality & Openness',
   'pages.card.description.3': 'Tiangong LCA Data Platform is committed to building a global interconnected network, realizing seamless data docking and promoting the efficient sharing and application of life cycle information.',
