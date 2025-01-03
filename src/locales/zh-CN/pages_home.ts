7d72ca0908378d827bdb2fa513d66ea2e4cba8b7
Zhangsi2 <22300087@muc.edu.cn>
2025-01-03 18:31:17 +0800

    Update welcome messages in English and Chinese localization files for clarity and impact

--- src/locales/zh-CN/pages_home.ts
+++ src/locales/zh-CN/pages_home.ts
@@ -1,6 +1,6 @@
 export default {
   'pages.name': '天工生命周期数据平台',
-  'pages.welcome': '欢迎使用天工生命周期数据平台',
+  'pages.welcome': '全球最大的开放生命周期数据平台',
   'pages.card.title.1': '可追溯 & 可信赖',
   'pages.card.description.1': '先进的技术和严格的质量控制，确保数据来源清晰、建模过程透明可追溯，为用户提供可信赖的生命周期数据。',
   'pages.card.title.2': '完整性 & 代表性',
