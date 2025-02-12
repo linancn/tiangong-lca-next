a27745ce7ffc0e39a1e847af79b87f241cb46ce1
hao <3105150874@qq.com>
2025-02-12 09:46:04 +0800

    defaultCpntext

--- src/contexts/unitContext.ts
+++ src/contexts/unitContext.ts
@@ -1,9 +1,11 @@
 import { createContext, useContext } from 'react';
+
 const UnitsContext: any = createContext(null);
+
 const useUnitsContext = () => {
     const context = useContext(UnitsContext);
     if (!context) {
-        console.log('UnitsContext 必须在 UnitsContext 内部使用');
+        return {units:[],setUnits:()=>{}};
     }
     return context;
 }
