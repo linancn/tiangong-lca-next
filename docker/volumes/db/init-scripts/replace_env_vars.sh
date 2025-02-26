cd672e49f1b1e1e15e615f441a5310bf5993de26
Biaoo <872299269@qq.com>
2025-02-26 20:20:53 +0800

    Enhance SQL environment variable replacement script

--- docker/volumes/db/init-scripts/replace_env_vars.sh
+++ docker/volumes/db/init-scripts/replace_env_vars.sh
@@ -3,15 +3,15 @@ set -e
 
 echo "Running replace_env_vars.sh script to substitute environment variables in SQL files..."
 
-# 使用sed替换SQL文件中的环境变量
+# To replace the environment variables in the SQL files
 if [ -f /docker-entrypoint-initdb.d/migrations/data.sql ]; then
     echo "Processing /docker-entrypoint-initdb.d/migrations/data.sql..."
     
-    # 创建一个临时文件
+    # Create a temporary file
     cp /docker-entrypoint-initdb.d/migrations/data.sql /tmp/data.sql.tmp
     
-    # 替换所有可能的环境变量
-    # 这里列出所有需要替换的环境变量
+    # Replace all possible environment variables
+    # Here is the list of all environment variables that need to be replaced
     sed -i "s|\${POSTGRES_PASSWORD}|$POSTGRES_PASSWORD|g" /tmp/data.sql.tmp
     sed -i "s|\${POSTGRES_DB}|$POSTGRES_DB|g" /tmp/data.sql.tmp
     sed -i "s|\${POSTGRES_HOST}|$POSTGRES_HOST|g" /tmp/data.sql.tmp
@@ -19,9 +19,9 @@ if [ -f /docker-entrypoint-initdb.d/migrations/data.sql ]; then
     sed -i "s|\${JWT_SECRET}|$JWT_SECRET|g" /tmp/data.sql.tmp
     sed -i "s|\${JWT_EXPIRY}|$JWT_EXPIRY|g" /tmp/data.sql.tmp
     sed -i "s|\${X_KEY}|$X_KEY|g" /tmp/data.sql.tmp
-    # 如果有其他环境变量需要替换，请在这里添加更多的sed命令
+    # If there are other environment variables that need to be replaced, please add more sed commands here
     
-    # 将处理后的文件内容写回原文件
+    # Write the processed file content back to the original file
     cat /tmp/data.sql.tmp > /docker-entrypoint-initdb.d/migrations/data.sql
     rm /tmp/data.sql.tmp
     
@@ -30,5 +30,4 @@ else
     echo "Warning: /docker-entrypoint-initdb.d/migrations/data.sql not found"
 fi
 
-# 脚本结束，不需要启动PostgreSQL
 echo "replace_env_vars.sh script completed" 
