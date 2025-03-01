b9daf17f386d01452fd07e7b25cfcff0ee88b4b1
hyy1987 <779003812@qq.com>
2025-03-01 06:46:41 +0000

    refactor: update form layout and replace Select with Input for language display

--- src/components/LangTextItem/form.tsx
+++ src/components/LangTextItem/form.tsx
@@ -31,7 +31,7 @@ const LangTextItemForm: FC<Props> = ({ name, label, rules }) => {
                 </Col>
                 <Col flex="auto" style={{ marginRight: '10px' }}>
                   <Form.Item noStyle name={[subField.name, '#text']} rules={rules ?? []}>
-                    <TextArea placeholder="text" rows={1} />
+                    <TextArea rows={1} />
                   </Form.Item>
                 </Col>
                 <Col flex="20px">
