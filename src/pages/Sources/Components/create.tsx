ca29a26fe7894de72d774e7642cb68b955f24385
hao <3105150874@qq.com>
2025-03-17 15:09:42 +0800

    fix:Replace SourceEdit with SourceCreate

--- src/pages/Sources/Components/create.tsx
+++ src/pages/Sources/Components/create.tsx
@@ -1,12 +1,14 @@
 import { initVersion } from '@/services/general/data';
 import { formatDateTime } from '@/services/general/util';
-import { createSource } from '@/services/sources/api';
+import { createSource, getSourceDetail } from '@/services/sources/api';
+import { genSourceFromData } from '@/services/sources/util';
+import { getThumbFileUrls } from '@/services/supabase/storage';
 import { supabaseStorageBucket } from '@/services/supabase/key';
 import { removeFile, uploadFile } from '@/services/supabase/storage';
 import styles from '@/style/custom.less';
-import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
+import { CloseOutlined, PlusOutlined , CopyOutlined } from '@ant-design/icons';
 import { ActionType, ProForm, ProFormInstance } from '@ant-design/pro-components';
-import { Button, Collapse, Drawer, Space, Tooltip, Typography, message } from 'antd';
+import { Button, Collapse, Drawer, Space, Tooltip, Typography, message,Spin } from 'antd';
 import path from 'path';
 import type { FC } from 'react';
 import { useCallback, useEffect, useRef, useState } from 'react';
@@ -17,9 +19,26 @@ import { SourceForm } from './form';
 type Props = {
   actionRef: React.MutableRefObject<ActionType | undefined>;
   lang: string;
+  actionType?: 'create' | 'copy' | 'createVersion';
+  id?: string;
+  version?: string;
 };
 
-const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
+// When type is 'copy' or 'createVersion', id and version are required parameters
+type CreateProps = 
+  | (Omit<Props, 'type'> & { actionType?: 'create' })
+  | (Omit<Props, 'type' | 'id' | 'version'> & { 
+      actionType: 'copy'; 
+      id: string; 
+      version: string;
+    })
+  | (Omit<Props, 'type' | 'id' | 'version'> & { 
+      actionType: 'createVersion'; 
+      id: string; 
+      version: string;
+    });
+
+const SourceCreate: FC<CreateProps> = ({ actionRef, lang, actionType='create', id, version }) => {
   const [drawerVisible, setDrawerVisible] = useState(false);
   const formRefCreate = useRef<ProFormInstance>();
   const [fromData, setFromData] = useState<any>({});
@@ -28,6 +47,7 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
   const [fileList0, setFileList0] = useState<any[]>([]);
   const [fileList, setFileList] = useState<any[]>([]);
   const [loadFiles, setLoadFiles] = useState<any[]>([]);
+  const [spinning, setSpinning] = useState<boolean>(false);
   const intl = useIntl();
 
   const handletFromData = () => {
@@ -78,7 +98,9 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
       });
     }
 
-    const result = await createSource(v4(), {
+    const paramsId = (actionType === 'createVersion' ? id : v4())??'';
+
+    const result = await createSource(paramsId, {
       ...fromData,
       sourceInformation: {
         ...fromData?.sourceInformation,
@@ -115,8 +137,32 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
     return true;
   };
 
+  const getFormDetail =async () => {
+    if(!id||!version)return;
+    setSpinning(true);
+    getSourceDetail(id, version).then(async (result: any) => {
+      const dataSet = genSourceFromData(result.data?.json?.sourceDataSet ?? {});
+      setInitData(dataSet);
+      setFromData(dataSet);
+      
+      const currentData = formRefCreate.current?.getFieldsValue();
+      formRefCreate.current?.setFieldsValue({...currentData, ...dataSet});
+
+      const initFile = await getThumbFileUrls(
+        dataSet.sourceInformation?.dataSetInformation?.referenceToDigitalFile,
+      );
+      await setFileList0(initFile);
+      await setFileList(initFile);
+      setSpinning(false);
+    });
+  };
+
   useEffect(() => {
     if (!drawerVisible) return;
+    if(actionType === 'copy'||actionType === 'createVersion'){
+      getFormDetail();
+      return;
+    };
 
     // const referenceToDataSetFormatId = 'a97a0155-0234-4b87-b4ce-a45da52f2a40';
     // getSourceDetail(referenceToDataSetFormatId, '').then(async (result2: any) => {
@@ -158,15 +204,20 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
 
   return (
     <>
-      <Tooltip title={<FormattedMessage id="pages.button.create" defaultMessage="Create" />}>
-        <Button
+      <Tooltip title={<FormattedMessage id={actionType === 'copy' ? 'pages.button.copy' : actionType === 'createVersion' ? 'pages.button.createVersion' : 'pages.button.create'} defaultMessage="Create" />}>
+        {
+          actionType === 'copy' ? (<Button shape="circle" icon={<CopyOutlined />} size="small" 
+            onClick={() => {
+            setDrawerVisible(true);
+          }} />):
+          (<Button
           size={'middle'}
           type="text"
           icon={<PlusOutlined />}
           onClick={() => {
             setDrawerVisible(true);
           }}
-        />
+        />)}
         {/* {buttonType === 'icon' ? (
                     <Button shape="circle" icon={<PlusOutlined />} size="small" onClick={() => setDrawerVisible(true)} />
                 ) : (
@@ -179,7 +230,7 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
         destroyOnClose={true}
         getContainer={() => document.body}
         title={
-          <FormattedMessage id="pages.source.drawer.title.create" defaultMessage="Create Source" />
+          <FormattedMessage id={actionType === 'copy' ? 'pages.source.drawer.title.copy' : actionType === 'createVersion' ? 'pages.source.drawer.title.createVersion' : 'pages.source.drawer.title.create'} defaultMessage="Create Source" />
         }
         width="90%"
         closable={false}
@@ -204,6 +255,7 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
           </Space>
         }
       >
+        <Spin spinning={spinning}>
         <ProForm
           formRef={formRefCreate}
           initialValues={initData}
@@ -230,6 +282,7 @@ const SourceCreate: FC<Props> = ({ actionRef, lang }) => {
             setFileList={setFileList}
           />
         </ProForm>
+        </Spin>
         <Collapse
           items={[
             {
