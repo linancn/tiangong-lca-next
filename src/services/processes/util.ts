774c268492a14d7d630c6dcd9aa70a8396d07c17
hao <3105150874@qq.com>
2025-04-03 09:40:22 +0800

    fix:Process field backfill problem

--- src/services/processes/util.ts
+++ src/services/processes/util.ts
@@ -1074,31 +1074,64 @@ export function genProcessFromData(data: any) {
         // ),
       },
       validation: {
-        review: {
-          '@type': data?.modellingAndValidation?.validation?.review?.['@type'] ?? {},
-          'common:reviewDetails': getLangList(
-            data?.modellingAndValidation?.validation?.review?.['common:reviewDetails'],
-          ),
-          'common:referenceToNameOfReviewerAndInstitution': {
-            '@refObjectId':
-              data?.modellingAndValidation?.validation?.review?.[
-                'common:referenceToNameOfReviewerAndInstitution'
-              ]?.['@refObjectId'] ?? {},
-            '@type':
-              data?.modellingAndValidation?.validation?.review?.[
-                'common:referenceToNameOfReviewerAndInstitution'
-              ]?.['@type'] ?? {},
-            '@uri':
-              data?.modellingAndValidation?.validation?.review?.[
-                'common:referenceToNameOfReviewerAndInstitution'
-              ]?.['@uri'] ?? {},
-            'common:shortDescription': getLangList(
-              data?.modellingAndValidation?.validation?.review?.[
-                'common:referenceToNameOfReviewerAndInstitution'
-              ]?.['common:shortDescription'],
-            ),
+        review: jsonToList(data?.modellingAndValidation?.validation?.review).map((review: any) => {
+          return {
+            '@type': review?.['@type'],
+            'common:scope': jsonToList(review?.['common:scope']),
+            'common:dataQualityIndicators': {
+              'common:dataQualityIndicator': jsonToList(
+                review?.['common:dataQualityIndicators']?.['common:dataQualityIndicator'],
+              ),
+            },
+            'common:reviewDetails': getLangList(review?.['common:reviewDetails']),
+            'common:referenceToNameOfReviewerAndInstitution': {
+              '@refObjectId':
+                review?.['common:referenceToNameOfReviewerAndInstitution']?.['@refObjectId'],
+              '@type': review?.['common:referenceToNameOfReviewerAndInstitution']?.['@type'],
+              '@uri': review?.['common:referenceToNameOfReviewerAndInstitution']?.['@uri'],
+              '@version': review?.['common:referenceToNameOfReviewerAndInstitution']?.['@version'],
+              'common:shortDescription': getLangList(
+                review?.['common:referenceToNameOfReviewerAndInstitution']?.[
+                  'common:shortDescription'
+                ],
+              ),
+            },
+            'common:otherReviewDetails': getLangList(review?.['common:otherReviewDetails']),
+            referenceToCompleteReviewReport: {
+              '@refObjectId': review?.['referenceToCompleteReviewReport']?.['@refObjectId'],
+              '@type': review?.['referenceToCompleteReviewReport']?.['@type'],
+              '@uri': review?.['referenceToCompleteReviewReport']?.['@uri'],
+              '@version': review?.['referenceToCompleteReviewReport']?.['@version'],
+              'common:shortDescription': getLangList(
+                review?.['referenceToCompleteReviewReport']?.['common:shortDescription'],
+              ),
+            },
+          };
+        }),
+      },
+      complianceDeclarations: {
+        compliance: jsonToList(data?.modellingAndValidation?.complianceDeclarations?.compliance).map(
+          (compliance: any) => {
+            return {
+              'common:referenceToComplianceSystem': {
+                '@refObjectId': compliance?.['common:referenceToComplianceSystem']?.['@refObjectId'],
+                '@type': compliance?.['common:referenceToComplianceSystem']?.['@type'],
+                '@uri': compliance?.['common:referenceToComplianceSystem']?.['@uri'],
+                '@version': compliance?.['common:referenceToComplianceSystem']?.['@version'],
+                'common:shortDescription': getLangList(
+                  compliance?.['common:referenceToComplianceSystem']?.['common:shortDescription'],
+                ),
+              },
+              'common:approvalOfOverallCompliance':
+                compliance?.['common:approvalOfOverallCompliance'],
+              'common:nomenclatureCompliance': compliance?.['common:nomenclatureCompliance'],
+              'common:methodologicalCompliance': compliance?.['common:methodologicalCompliance'],
+              'common:reviewCompliance': compliance?.['common:reviewCompliance'],
+              'common:documentationCompliance': compliance?.['common:documentationCompliance'],
+              'common:qualityCompliance': compliance?.['common:qualityCompliance'],
+            };
           },
-        },
+        ),
       },
     },
     administrativeInformation: {
@@ -1474,66 +1507,6 @@ export function genProcessFromData(data: any) {
           };
         }
       }),
-    },
-    validation: {
-      review: jsonToList(data?.modellingAndValidation?.validation?.review).map((review: any) => {
-        return {
-          '@type': review?.['@type'],
-          'common:scope': jsonToList(review?.['common:scope']),
-          'common:dataQualityIndicators': {
-            'common:dataQualityIndicator': jsonToList(
-              review?.['common:dataQualityIndicators']?.['common:dataQualityIndicator'],
-            ),
-          },
-          'common:reviewDetails': getLangList(review?.['common:reviewDetails']),
-          'common:referenceToNameOfReviewerAndInstitution': {
-            '@refObjectId':
-              review?.['common:referenceToNameOfReviewerAndInstitution']?.['@refObjectId'],
-            '@type': review?.['common:referenceToNameOfReviewerAndInstitution']?.['@type'],
-            '@uri': review?.['common:referenceToNameOfReviewerAndInstitution']?.['@uri'],
-            '@version': review?.['common:referenceToNameOfReviewerAndInstitution']?.['@version'],
-            'common:shortDescription': getLangList(
-              review?.['common:referenceToNameOfReviewerAndInstitution']?.[
-                'common:shortDescription'
-              ],
-            ),
-          },
-          'common:otherReviewDetails': getLangList(review?.['common:otherReviewDetails']),
-          referenceToCompleteReviewReport: {
-            '@refObjectId': review?.['referenceToCompleteReviewReport']?.['@refObjectId'],
-            '@type': review?.['referenceToCompleteReviewReport']?.['@type'],
-            '@uri': review?.['referenceToCompleteReviewReport']?.['@uri'],
-            '@version': review?.['referenceToCompleteReviewReport']?.['@version'],
-            'common:shortDescription': getLangList(
-              review?.['referenceToCompleteReviewReport']?.['common:shortDescription'],
-            ),
-          },
-        };
-      }),
-    },
-    complianceDeclarations: {
-      compliance: jsonToList(data?.modellingAndValidation?.complianceDeclarations?.compliance).map(
-        (compliance: any) => {
-          return {
-            'common:referenceToComplianceSystem': {
-              '@refObjectId': compliance?.['common:referenceToComplianceSystem']?.['@refObjectId'],
-              '@type': compliance?.['common:referenceToComplianceSystem']?.['@type'],
-              '@uri': compliance?.['common:referenceToComplianceSystem']?.['@uri'],
-              '@version': compliance?.['common:referenceToComplianceSystem']?.['@version'],
-              'common:shortDescription': getLangList(
-                compliance?.['common:referenceToComplianceSystem']?.['common:shortDescription'],
-              ),
-            },
-            'common:approvalOfOverallCompliance':
-              compliance?.['common:approvalOfOverallCompliance'],
-            'common:nomenclatureCompliance': compliance?.['common:nomenclatureCompliance'],
-            'common:methodologicalCompliance': compliance?.['common:methodologicalCompliance'],
-            'common:reviewCompliance': compliance?.['common:reviewCompliance'],
-            'common:documentationCompliance': compliance?.['common:documentationCompliance'],
-            'common:qualityCompliance': compliance?.['common:qualityCompliance'],
-          };
-        },
-      ),
     },
   });
 }
