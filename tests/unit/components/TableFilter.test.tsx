f61ed40202e2ac62e57ce0d590e6bef4edae7960
ForeverYoung5 <3105150874@qq.com>
2026-03-06 19:28:30 +0800

    feat: Update TableFilter test to reflect changes in localization for 'All' option

--- tests/unit/components/TableFilter.test.tsx
+++ tests/unit/components/TableFilter.test.tsx
@@ -13,7 +13,7 @@ type TableFilterProps = {
 
 jest.mock('umi', () => {
   const messages: Record<string, string> = {
-    'pages.table.filter.all': 'All',
+    'pages.table.filter.all.reviewType': 'Type of review',
     'pages.table.filter.unreviewed': 'Unreviewed',
     'pages.table.filter.reviewing': 'Reviewing',
     'pages.table.filter.reviewed': 'Reviewed',
@@ -93,7 +93,7 @@ describe('TableFilter Component', () => {
 
     const options = within(select).getAllByRole('option');
     expect(options.map((option) => option.textContent)).toEqual([
-      'All',
+      'Type of review',
       'Unreviewed',
       'Reviewing',
       'Reviewed',
