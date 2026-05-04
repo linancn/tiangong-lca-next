cba40d2affaa241b27f28f1db5a69674d5ae50b2
ForeverYoung5 <3105150874@qq.com>
2026-05-04 14:09:19 +0800

    fix(types): align dataset tabs with sdk extensions

--- src/services/lifeCycleModels/data.ts
+++ src/services/lifeCycleModels/data.ts
@@ -43,7 +43,7 @@ export type LifeCycleModelDataSetObjectKeys = Exclude<
       ? K
       : never;
   }[keyof LifeCycleModel['lifeCycleModelDataSet']],
-  undefined
+  undefined | 'common:other'
 >;
 
 export type FormLifeCycleModel = Pick<
